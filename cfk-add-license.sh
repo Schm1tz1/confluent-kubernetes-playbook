0927d5b2300360bbd517b4bfd7aaf7461c602527
Roman Schmitz <Schm1tz1@users.noreply.github.com>
2022-07-29 09:30:56 +0200

    Delete cfk-add-license.sh

--- cfk-add-license.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -n "license=<paste your key here>" > license.txt
-kubectl create secret generic confluent-license \
-  --from-file=license.txt=./license.txt \
-  --namespace confluent
-
