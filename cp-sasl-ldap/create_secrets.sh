5d78f2dbf8ac5f4bcb9b454972174fcf1f0af589
root <root@Jumphost.fritz.box>
2022-10-20 22:19:32 +0200

    added ladp authn example and kcat test container

---
+++ cp-sasl-ldap/create_secrets.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+
+# clean up
+# kubectl -n confluent delete secret credentials
+# kubectl -n confluent delete secret credentials
+
+kubectl create secret generic credentials \
+    --from-file=ldap.txt=./credentials/ldap.txt \
+    -n confluent
+
+kubectl create secret generic kcat-config \
+    --from-file=kcat.conf=./kcat.conf \
+    -n confluent
