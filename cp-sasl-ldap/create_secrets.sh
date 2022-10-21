2e22e6c65fa916f9b2807f5e62a579fd4639af88
root <root@Jumphost.fritz.box>
2022-10-21 13:22:55 +0200

    Update on kcat pod: configMap included and non-restart policy

--- cp-sasl-ldap/create_secrets.sh
+++ cp-sasl-ldap/create_secrets.sh
@@ -3,7 +3,7 @@ set -x
 
 # clean up
 # kubectl -n confluent delete secret credentials
-# kubectl -n confluent delete secret credentials
+# kubectl -n confluent delete secret kcat-config
 
 kubectl create secret generic credentials \
     --from-file=ldap.txt=./credentials/ldap.txt \
