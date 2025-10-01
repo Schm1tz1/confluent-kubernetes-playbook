b6d7e198e8cf7c46ef8d82ac933e5730e75356a2
Roman Schmitz <roman.schmitz@gmail.com>
2025-10-01 09:22:12 +0000

    New LDAP container with cleaner structure, multiple subtrees added.

--- cp-rbac-ldap/openldap_install.sh
+++ cp-rbac-ldap/openldap_install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-helm upgrade --install -f ./openldap/ldaps-rbac.yaml test-ldap ./openldap/ --namespace confluent
-
-# LDAP split-setup - not working yet
-# helm upgrade --install -f ./openldap/ldaps-rbac.yaml test-ldap ./openldap/ --values ./openldap/values-split.yaml --namespace confluent
+kubectl apply -f openldap-certs.yaml
+kubectl apply -n confluent -f openldap.yaml
--- cp-rbac-ldap/openldap_test_ingress.sh
+++ cp-rbac-ldap/openldap_test_ingress.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -x
-ldapsearch -LLL -x -H ldaps://ldap.k8s.internal.schmitzi.net:443 -b 'dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!'
+ldapsearch -LLL -x -H ldaps://ldap.k8s.internal.schmitzi.net:443 -b 'ou=users,dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!'
