eb3a6a2acec75164ba7a2dcf669a3e5b72d1537e
Roman Schmitz <roman.schmitz@gmail.com>
2025-11-19 13:39:40 +0000

    Sidecar template added.

--- hybrid/create_configs_ccloud.sh
+++ hybrid/create_configs_ccloud.sh
@@ -5,11 +5,11 @@
 #
 
 kubectl create secret generic ccloud-credentials \
-    --namespace=confluent \
+    --namespace=ccloud \
     --from-file=plain.txt=ccloud-credentials.txt \
     -o yaml --dry-run=client | kubectl apply -f -
 
 kubectl create secret generic ccloud-sr-credentials \
-    --namespace=confluent \
+    --namespace=ccloud \
     --from-file=basic.txt=ccloud-sr-credentials.txt \
     -o yaml --dry-run=client | kubectl apply -f -
--- hybrid/create_configs_csm.sh
+++ hybrid/create_configs_csm.sh
@@ -5,11 +5,11 @@
 #
 
 kubectl create secret generic csm-sidecar-properties \
-    --namespace=confluent \
+    --namespace=ccloud \
     --from-file=csm.properties=csm-sidecar-azure-ple-wrapper-asymm-AES_GCM.properties \
     -o yaml --dry-run=client | kubectl apply -f -
 
 kubectl create configmap csm-enabled \
-    --namespace=confluent \
+    --namespace=ccloud \
     --from-file=pod-template.yaml=csm-enabled-template.yaml \
     -o yaml --dry-run=client | kubectl apply -f -
