e00eb677344c5afc89dfcab816c1e211b8d6e02f
Roman Schmitz <roman.schmitz@gmail.com>
2026-05-29 15:42:48 +0000

    Pertest updated, added test job for S3 listing with/withour versions due to observations with HCP storage.

--- tiered-storage/install_minio.sh
+++ tiered-storage/install_minio.sh
@@ -10,3 +10,34 @@ helm install minio minio/minio -n minio\
     --create-namespace
 
 kubectl exec -it deploy/minio -n minio -- bash -c "mc alias set local http://localhost:9000 rootuser rootpass123 && mc mb local/cflt-tier"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: minio-ingress
+  namespace: minio
+spec:
+  tls:
+  - hosts:
+    - minio.k8s.schmitzi.net
+    secretName: tls-schmitzi-ingress
+  rules:
+  - host: minio.k8s.schmitzi.net
+    http:
+      paths:
+      - path: /api/(.*)
+        pathType: ImplementationSpecific
+        backend:
+          service:
+            name: minio
+            port:
+              number: 9000
+      - path: /console/(.*)
+        pathType: ImplementationSpecific
+        backend:
+          service:
+            name: minio-console
+            port:
+              number: 9001
+EOF
